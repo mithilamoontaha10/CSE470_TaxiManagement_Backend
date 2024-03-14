46dcb2ba0feb4524e8f6eaded5ca8bb947b2cb1b
arianwazed <148196534+arianwazed@users.noreply.github.com>
2024-03-14 19:15:48 +0600

    Update index.js

--- TaxiManagement_serverSide - Copy/index.js
+++ TaxiManagement_serverSide - Copy/index.js
@@ -267,7 +267,14 @@ async function run() {
             res.send(result)
         })
 
+        // Driver see the Driving Request status
+        app.get("/driver/driveReqStatus/:email", async(req,res)=>{
+            const emailofDriver = req.params.email;
+            const query= {email: emailofDriver}
+            const result = await DrivingRequestCollections.find(query).toArray()
+            res.send(result);
 
+        })
         
 
 
