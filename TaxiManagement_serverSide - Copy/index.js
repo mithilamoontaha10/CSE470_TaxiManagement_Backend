25ec061026906a1d01f8507bbd607338cb05ccba
marjukahmed87 <151629792+marjukahmed87@users.noreply.github.com>
2024-03-28 00:53:34 +0600

    Update index.js

--- TaxiManagement_serverSide - Copy/index.js
+++ TaxiManagement_serverSide - Copy/index.js
@@ -464,6 +464,16 @@ async function run() {
             res.send(result)
 
         })
+        // payment
+
+        // passenger Make Payment:
+
+        app.post("/passenger/makePayment", async(req,res)=>{
+            const data = req.body;
+            
+            const result = await PaymentCollections.insertOne(data)
+            res.send(result)
+        })
         // Send a ping to confirm a successful connection
         await client.db("admin").command({ ping: 1 });
         console.log("Pinged your deployment. You successfully connected to MongoDB!");
